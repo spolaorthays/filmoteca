5b84ef1815147535ff6d4907061bd3222d7306d0
Thays Spolaor <thays.spolaor@gmail.com>
2024-01-07 16:11:09 -0300

    Migração das imagens para o shared e ajuste no tamanho dos ícones de gênero

--- moviedetails/src/main/java/br/com/spolaorthays/filmoteca/moviedetails/presentation/adapter/DetailsGenreRecyclerViewAdapter.kt
+++ moviedetails/src/main/java/br/com/spolaorthays/filmoteca/moviedetails/presentation/adapter/DetailsGenreRecyclerViewAdapter.kt
@@ -4,7 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import br.com.spolaorthays.filmoteca.moviedetails.R
+import br.com.spolaorthays.filmoteca.shared.R as Rshared
 import br.com.spolaorthays.filmoteca.moviedetails.databinding.GenreContentItemBinding
 import br.com.spolaorthays.filmoteca.shared.model.MovieGenre
 import com.squareup.picasso.Picasso
@@ -33,33 +33,33 @@ class DetailsGenreRecyclerViewAdapter(private var genres: List<MovieGenre>) :
                 genreName.text = genre.name
                 Picasso.get()
                     .load(getIconByName(genre.name))
-                    .resize(100, 100)
+                    .resize(70, 70)
                     .into(genreIcon)
             }
         }
 
         private fun getIconByName(genreName: String): Int {
             return when (genreName) {
-                "Ação" -> R.drawable.ic_action
-                "Aventura" -> R.drawable.ic_adventure
-                "Animação" -> R.drawable.ic_animation
-                "Comédia" -> R.drawable.ic_comedy
-                "Crime" -> R.drawable.ic_crime
-                "Documentário" -> R.drawable.ic_documentary
-                "Drama" -> R.drawable.ic_drama
-                "Família" -> R.drawable.ic_family
-                "Fantasia" -> R.drawable.ic_fantasy
-                "História" -> R.drawable.ic_history
-                "Terror" -> R.drawable.ic_horror
-                "Música" -> R.drawable.ic_music
-                "Mistério" -> R.drawable.ic_music
-                "Romance" -> R.drawable.ic_romance
-                "Ficção Científica", "Ficção científica" -> R.drawable.ic_cientific_fiction
-                "Filme de TV" -> R.drawable.ic_television
-                "Thriller" -> R.drawable.ic_thriller
-                "Guerra" -> R.drawable.ic_war
-                "Faroeste", "Ocidental" -> R.drawable.ic_western
-                else -> R.drawable.ic_crime_2
+                "Ação" -> Rshared.drawable.ic_action
+                "Aventura" -> Rshared.drawable.ic_adventure
+                "Animação" -> Rshared.drawable.ic_animation
+                "Comédia" -> Rshared.drawable.ic_comedy
+                "Crime" -> Rshared.drawable.ic_crime
+                "Documentário" -> Rshared.drawable.ic_documentary
+                "Drama" -> Rshared.drawable.ic_drama
+                "Família" -> Rshared.drawable.ic_family
+                "Fantasia" -> Rshared.drawable.ic_fantasy
+                "História" -> Rshared.drawable.ic_history
+                "Terror" -> Rshared.drawable.ic_horror
+                "Música" -> Rshared.drawable.ic_music
+                "Mistério" -> Rshared.drawable.ic_music
+                "Romance" -> Rshared.drawable.ic_romance
+                "Ficção Científica", "Ficção científica" -> Rshared.drawable.ic_cientific_fiction
+                "Filme de TV" -> Rshared.drawable.ic_television
+                "Thriller" -> Rshared.drawable.ic_thriller
+                "Guerra" -> Rshared.drawable.ic_war
+                "Faroeste", "Ocidental" -> Rshared.drawable.ic_western
+                else -> Rshared.drawable.ic_crime_2
             }
         }
     }
